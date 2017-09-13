215ba9ef0f77bb09fcd42514d17351fdf8bcfbdd
Martin Tournoij <martin@arp242.net>
2017-09-13 22:14:22 +0100

    Refactor a bit
    Paying the debt of my 5am coding efforts from yesterday >_<

--- godocgen.go
+++ godocgen.go
@@ -12,10 +12,6 @@ import (
 	"strings"
 )
 
-type packageT struct {
-	Name, Doc, Depth string
-}
-
 var (
 	templates = template.Must(template.ParseFiles("package.tmpl", "index.tmpl", "home.tmpl"))
 	style     = mustRead("./style.css")
@@ -26,7 +22,7 @@ var (
 		Packages   []packageT
 	}{
 		{
-			"libraries",
+			"Libraries",
 			"Generic libraries that can be used by any project. Both public and private.",
 			[]string{
 				"cache", "database", "dlm", "flipout", "go-spamc", "httperr",
@@ -36,7 +32,7 @@ var (
 			[]packageT{},
 		},
 		{
-			"desk",
+			"Desk",
 			"Teamwork Desk-specific projects",
 			[]string{
 				"desk", "deskactivity", "deskdocs", "deske2e", "deskedge",
@@ -46,7 +42,7 @@ var (
 			[]packageT{},
 		},
 		{
-			"projects",
+			"Projects",
 			"Teamwork Projects-specific projects.",
 			[]string{
 				"TeamworkAPIInGO", "projects-api", "projects-webhooks", "projectsapigo", "notification-server",
@@ -54,13 +50,13 @@ var (
 			[]packageT{},
 		},
 		{
-			"other",
+			"Other",
 			"Everything not in one of the other groups.",
 			[]string{},
 			[]packageT{},
 		},
 		{
-			"deprecated",
+			"Deprecated",
 			"Old stuff; don't use unless you really know what you're doing",
 			[]string{
 				"TeamworkDeskTool", "go-modules", "email",
@@ -70,193 +66,70 @@ var (
 	}
 )
 
+type packageT struct {
+	Name           string // Top-level name
+	Doc            string // Single-line comment
+	Depth          int    // Depth to display it as
+	FullImportPath string // Full import path
+	RelImportPath  string // Relative import path (may be the same as Full)
+}
+
+// Config for godocgen.
+type Config struct {
+	Outdir     string
+	Clonedir   string
+	Scan       []string
+	RelativeTo string
+	MainTitle  string // Title to add in the header, <title> tag, and some other places.
+
+	packages []packageT
+}
+
 func main() {
-	outdir := "./_site"
-	scan := []string{"github.com/teamwork/..."}
-	if len(os.Args) > 2 {
-		scan = os.Args[2:]
+	c := Config{
+		Outdir:     "./_site",
+		Clonedir:   "./_clone",
+		Scan:       []string{"github.com/teamwork/..."},
+		RelativeTo: "github.com/teamwork",
+		MainTitle:  "Teamwork Go doc",
 	}
 
-	packages, err := list(scan...)
+	packages, err := list(c)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "cannot list packages: %v\n", err)
 		os.Exit(1)
 	}
 
 	for _, pkg := range packages {
-		err := writePackage(outdir, pkg)
+		err := writePackage(c, pkg)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "could not write package %v: %v\n", pkg, err)
 			os.Exit(1)
 		}
 	}
 
-	if err := makeIndexes(outdir); err != nil {
+	if err := makeIndexes(c); err != nil {
+		fmt.Fprintf(os.Stderr, "could not generate index.html files: %v\n", err)
+		os.Exit(1)
+	}
+	if err := makeHome(c, packages); err != nil {
 		fmt.Fprintf(os.Stderr, "could not generate index.html files: %v\n", err)
 		os.Exit(1)
 	}
 
-	if err := makeHome(outdir, scan); err != nil {
-		fmt.Fprintf(os.Stderr, "could not generate index.html files: %v\n", err)
-		os.Exit(1)
-	}
-
-}
-
-func mustRead(path string) string {
-	fp, err := os.Open(path)
-	if err != nil {
-		panic(err)
-	}
-
-	out, err := ioutil.ReadAll(fp)
-	if err != nil {
-		panic(err)
-	}
-
-	return string(out)
-}
-
-// list all packages in "dirs", where every entry is an argument to "go list".
-func list(dirs ...string) ([]string, error) {
-	packages := []string{}
-
-	for _, dir := range dirs {
-		cmd := exec.Command("go", "list", dir)
-
-		out, err := cmd.Output()
-		if err != nil {
-			return nil, err
-		}
-
-		packages = append(packages, strings.Split(strings.TrimSpace(string(out)), "\n")...)
-	}
-
-	return packages, nil
-}
-
-func listDocs(dirs ...string) ([][]string, error) {
-	packages := [][]string{}
-
-	for _, dir := range dirs {
-		cmd := exec.Command("go", "list", "-f", "{{.ImportPath}} {{.Doc}}", dir)
-		out, err := cmd.Output()
-		if err != nil {
-			return nil, fmt.Errorf("listDocs error: %v; output: %s", err, out)
-		}
-
-		pkgs := strings.Split(strings.TrimSpace(string(out)), "\n")
-		for _, importPath := range pkgs {
-			p := strings.Replace(importPath, "github.com/teamwork/", "", 1)
-			space := strings.Index(p, " ")
-			if space == -1 {
-				packages = append(packages, []string{p, "", "0", importPath})
-			} else {
-				packages = append(packages, []string{p[:space], p[space:], "0", importPath})
-			}
-		}
-	}
-
-	// go list won't list dirs without Go files, so e.g.
-	// github.com/teamwork/TeamworkAPIInGO/app
-	// Would show as just "app", since "TeamworkAPIInGO" has no Go files.
-	add := [][]string{}
-	for _, p := range packages {
-		d := filepath.Dir(p[0])
-		if d == "." {
-			continue
-		}
-
-		goFiles, _ := filepath.Glob(filepath.Join("./_clone/src/github.com/teamwork/", d, "/*.go"))
-		if len(goFiles) > 0 {
-			continue
-		}
-
-		found := false
-		for _, a := range add {
-			if a[0] == d {
-				found = true
-				break
-			}
-		}
-		if found {
-			continue
-		}
-
-		add = append(add, []string{d, "",
-			// I think I have officially discovered the best way of doing
-			// subtraction in Go.
-			fmt.Sprintf("%s", []byte{[]byte(p[2])[0] - 1}),
-			d})
-	}
-
-	packages = append(packages, add...)
-
-	sort.Slice(packages, func(i, j int) bool {
-		return packages[i][0] < packages[j][0]
-	})
-
-	// Reduce to just package name and record indent level.
-	for i := range packages {
-		packages[i][2] = fmt.Sprintf("%d", len(strings.Split(packages[i][0], "/")))
-		packages[i][0] = filepath.Base(packages[i][0])
-
-		packages[i][3] = strings.Replace(strings.TrimSpace(packages[i][3]), "github.com/teamwork/", "", 1)
-	}
-
-	// Indent level 0 for packages with 0 subpackages
-	for i := range packages {
-		if i+1 == len(packages) {
-			break
-		}
-		if packages[i][2] != "1" {
-			continue
-		}
-		if packages[i+1][2] == "1" {
-			packages[i][2] = "0"
-		}
-	}
-
-	return packages, nil
-}
-
-// godoc runs godoc on a package and gets the result.
-func godoc(pkg string) (string, error) {
-	// https://go.googlesource.com/tools/+/2d19ab38faf14664c76088411c21bf4fafea960b/godoc/static/
-	cmd := exec.Command("godoc", "-html", "-templates", "godoc_tpl", pkg)
-	out, err := cmd.Output()
-	if err != nil {
-		return "", err
-	}
-
-	// Remove the first line, which is always:
-	//    use 'godoc cmd/fmt' for documentation on the fmt command
-	// and always unwanted.
-	doc := string(out)
-	return doc[strings.Index(doc, "\n"):], nil
 }
 
 // Write package documentation.
-func writePackage(outdir, pkg string) error {
-	doc, err := godoc(pkg)
+func writePackage(c Config, pkg packageT) error {
+	doc, err := godoc(pkg.FullImportPath)
 	if err != nil {
 		return err
 	}
 
-	out := filepath.Join(outdir, filepath.Dir(pkg), filepath.Base(pkg)) + "/index.html"
-	relto := "github.com/teamwork/"
-	if relto != "" {
-		relpkg := pkg[len(relto):]
-		if relpkg == relto {
-			return nil
-		}
-		out = filepath.Join(outdir, filepath.Dir(relpkg), filepath.Base(relpkg)) + "/index.html"
-	}
-
+	out := filepath.Join(c.Outdir, filepath.Dir(pkg.RelImportPath), pkg.Name) + "/index.html"
 	if err := os.MkdirAll(filepath.Dir(out), 0700); err != nil {
 		return err
 	}
-
 	fp, err := os.Create(out)
 	if err != nil {
 		return err
@@ -266,9 +139,8 @@ func writePackage(outdir, pkg string) error {
 	err = templates.ExecuteTemplate(buf, "package.tmpl", map[string]interface{}{
 		"style":     template.CSS(style),
 		"godoc":     template.HTML(doc),
-		"pkgHead":   filepath.Base(pkg),
-		"pkgFull":   pkg,
-		"mainTitle": "Teamwork Go doc",
+		"mainTitle": c.MainTitle,
+		"pkg":       pkg,
 	})
 	if err != nil {
 		return err
@@ -284,10 +156,26 @@ func writePackage(outdir, pkg string) error {
 	return nil
 }
 
+// godoc runs godoc on a package and gets the result.
+func godoc(path string) (string, error) {
+	// https://go.googlesource.com/tools/+/2d19ab38faf14664c76088411c21bf4fafea960b/godoc/static/
+	cmd := exec.Command("godoc", "-html", "-templates", "godoc_tpl", path)
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+
+	// Remove the first line, which is always:
+	//    use 'godoc cmd/fmt' for documentation on the fmt command
+	// and always unwanted.
+	doc := string(out)
+	return doc[strings.Index(doc, "\n"):], nil
+}
+
 // Create indexes for packages that don't have one; this happens if there's a
 // subdir without any go files.
-func makeIndexes(outdir string) error {
-	return filepath.Walk(outdir, func(path string, info os.FileInfo, err error) error {
+func makeIndexes(c Config) error {
+	return filepath.Walk(c.Outdir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
@@ -307,7 +195,7 @@ func makeIndexes(outdir string) error {
 			return fmt.Errorf("cannot get Abs of %v: %v", path, err)
 		}
 
-		if err := makeIndex(path); err != nil {
+		if err := makeIndex(c, path); err != nil {
 			return fmt.Errorf("cannot make index for %v: %v", path, err)
 		}
 
@@ -316,25 +204,15 @@ func makeIndexes(outdir string) error {
 }
 
 // Make the homepage.
-func makeHome(path string, pkgs []string) error {
-	// Get list of all files and dirs.
-	contents, err := listDocs(pkgs...)
-	if err != nil {
-		return err
-	}
-
+func makeHome(c Config, packages []packageT) error {
 	// Add to group.
-	for _, pkg := range contents {
+	for _, pkg := range packages {
 		found := false
 	groups:
 		for i, g := range groups {
 			for _, project := range g.Projects {
-				if strings.HasPrefix(pkg[3], project) {
-					groups[i].Packages = append(groups[i].Packages, packageT{
-						Name:  pkg[0],
-						Doc:   pkg[1],
-						Depth: pkg[2],
-					})
+				if strings.HasPrefix(pkg.RelImportPath, project) {
+					groups[i].Packages = append(groups[i].Packages, pkg)
 					found = true
 					break groups
 				}
@@ -342,15 +220,11 @@ func makeHome(path string, pkgs []string) error {
 		}
 
 		if !found {
-			groups[3].Packages = append(groups[3].Packages, packageT{
-				Name:  pkg[0],
-				Doc:   pkg[1],
-				Depth: pkg[2],
-			})
+			groups[3].Packages = append(groups[3].Packages, pkg)
 		}
 	}
 
-	out := filepath.Join(path, "/index.html")
+	out := filepath.Join(c.Outdir, "/index.html")
 	fp, err := os.Create(out)
 	if err != nil {
 		return err
@@ -360,7 +234,7 @@ func makeHome(path string, pkgs []string) error {
 	err = templates.ExecuteTemplate(buf, "home.tmpl", map[string]interface{}{
 		"style":     template.CSS(style),
 		"groups":    groups,
-		"mainTitle": "Teamwork Go doc",
+		"mainTitle": c.MainTitle,
 	})
 	if err != nil {
 		return err
@@ -376,7 +250,7 @@ func makeHome(path string, pkgs []string) error {
 	return nil
 }
 
-func makeIndex(path string) error {
+func makeIndex(c Config, path string) error {
 	// Get list of all files and dirs.
 	contents, err := ioutil.ReadDir(path)
 	if err != nil {
@@ -391,12 +265,9 @@ func makeIndex(path string) error {
 
 	buf := bufio.NewWriter(fp)
 	err = templates.ExecuteTemplate(buf, "index.tmpl", map[string]interface{}{
-		"style":    template.CSS(style),
-		"contents": contents,
-		//"godoc":     template.HTML(doc),
-		//"pkgHead":   filepath.Base(pkg),
-		//"pkgFull":   pkg,
-		"mainTitle": "Teamwork Go doc",
+		"style":     template.CSS(style),
+		"contents":  contents,
+		"mainTitle": c.MainTitle,
 	})
 	if err != nil {
 		return err
@@ -410,4 +281,122 @@ func makeIndex(path string) error {
 	}
 
 	return nil
+}
+
+func run(cmd ...string) (stdout []string, stderr []string, err error) {
+	r := exec.Command(cmd[0], cmd[1:]...)
+
+	// TODO: Read stderr too
+	out, err := r.Output()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return strings.Split(strings.Trim(string(out), "\n"), "\n"), nil, nil
+}
+
+func removePathPrefix(full, remove string) string {
+	return strings.Trim(strings.Replace(full, remove, "", 1), "/")
+}
+
+func mustRead(path string) string {
+	fp, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+
+	out, err := ioutil.ReadAll(fp)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(out)
+}
+
+func list(c Config) ([]packageT, error) {
+	packages := []packageT{}
+
+	for _, dir := range c.Scan {
+		stdout, stderr, err := run("go", "list", "-f", "{{.ImportPath}} {{.Doc}}", dir)
+		if err != nil {
+			return nil, fmt.Errorf("go list error: %v; stderr: %v", err, stderr)
+		}
+
+		for _, line := range stdout {
+			space := strings.Index(line, " ")
+			pkg := packageT{}
+
+			if space == -1 {
+				pkg.FullImportPath = strings.TrimSpace(line)
+			} else {
+				pkg.FullImportPath = line[:space]
+				pkg.Doc = line[space+1:]
+			}
+			pkg.RelImportPath = pkg.FullImportPath
+			if c.RelativeTo != "" {
+				pkg.RelImportPath = removePathPrefix(pkg.FullImportPath, c.RelativeTo)
+			}
+			pkg.Name = filepath.Base(pkg.RelImportPath)
+			pkg.Depth = len(strings.Split(pkg.RelImportPath, "/"))
+
+			packages = append(packages, pkg)
+		}
+	}
+
+	// The "go list" tool doesn't list directories without any *.go files; we
+	// need to add them since we need it for the UI.
+	add := []packageT{}
+	for _, p := range packages {
+		up := filepath.Dir(p.FullImportPath)
+		if up == "." || up == c.RelativeTo {
+			continue
+		}
+
+		//fmt.Printf("%#v\n", p)
+
+		path := filepath.Join(c.Clonedir, "/src/", up, "/*.go")
+		goFiles, _ := filepath.Glob(path)
+		if len(goFiles) > 0 {
+			continue
+		}
+
+		// Don't add duplicates.
+		found := false
+		for _, a := range add {
+			if a.FullImportPath == up {
+				found = true
+				break
+			}
+		}
+		if found {
+			continue
+		}
+
+		add = append(add, packageT{
+			Name:           filepath.Base(up),
+			FullImportPath: up,
+			RelImportPath:  removePathPrefix(up, c.RelativeTo),
+			Depth:          p.Depth - 1,
+		})
+	}
+	packages = append(packages, add...)
+
+	sort.Slice(packages, func(i, j int) bool {
+		return packages[i].FullImportPath < packages[j].FullImportPath
+	})
+
+	// Packages without any subpackages are indent level 0.
+	for i := range packages {
+		if i+1 == len(packages) {
+			break
+		}
+		if packages[i].Depth != 1 {
+			continue
+		}
+		if packages[i+1].Depth == 1 {
+			packages[i].Depth = 0
+		}
+	}
+
+	return packages, nil
 }
